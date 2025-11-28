8847fd92915d2d17966036ac914b5e7012d4d95f
zhch <zhchqingdao@163.com>
2025-11-28 15:21:32 +0800

    replace selection or total

--- src/commands/my-command.ts
+++ src/commands/my-command.ts
@@ -1,12 +1,28 @@
 import { Editor } from "obsidian";
 
 /**
+ * 如果选中了内容就替换选中的内容， 没有选中就替换全部的内容
+ * @param editor
+ * @param replaceFun 替换函数， 把内容进行转换
+ */
+function replaceContent(editor: Editor, replaceFun: (e: string) => string) {
+	let content = editor.getSelection();
+	const hasSelection = content.length > 0;
+	if (hasSelection) {
+		const replaced = replaceFun(content); // 两个 newlines 替换为一个
+		editor.replaceSelection(replaced);
+	} else {
+		content = editor.getValue(); // 取得全文
+		const replaced = replaceFun(content); // 两个 newlines 替换为一个
+		editor.setValue(replaced); // 设置全文
+	}
+}
+
+/**
  * 把编辑器的连续两个换行全部换成一个换行
  */
 export function mergeDoubleNewLines(editor: Editor) {
-	const content = editor.getValue(); // 取得全文
-	const replaced = content.replace(/\n\n/g, "\n"); // 两个 newlines 替换为一个
-	editor.setValue(replaced); // 设置全文
+	replaceContent(editor, (text) => text.replace(/\n\n/g, "\n123"));
 }
 
 /**
@@ -14,12 +30,7 @@ export function mergeDoubleNewLines(editor: Editor) {
  * @param editor
  */
 export function removeLinks(editor: Editor) {
-	// 1. 获取当前内容
-	const content = editor.getValue();
-
-	// 2. 移除所有 Markdown 链接，仅保留文本部分
-	const newContent = content.replace(/\[([^\]]+)\]\(([^)]+)\)/g, "$1");
-
-	// 3. 设置回编辑器
-	editor.setValue(newContent);
+	replaceContent(editor, (text) =>
+		text.replace(/\[([^\]]+)\]\(([^)]+)\)/g, "$1"),
+	);
 }
